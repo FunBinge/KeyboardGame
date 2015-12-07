373d89c1e607583adbac0b1b4f54cabb9f9a1850
Ed <cardenas.eduardo10@yahoo.com.au>
2015-12-07 20:25:11 +1030

    Added keyboard UI
    +KeyboardInputListener
    +UIKeyboard
    +UIKey

---
+++ Assets/Scripts/KeyBoardInputListener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBoardInputListener : MonoBehaviour {
+
+    public delegate void KeyPressedAction(char keyEntered);
+    public static event KeyPressedAction OnKeyInputReceived;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.CurrentGameState == GameManager.GameState.IN_GAME && Input.anyKeyDown && !Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1))
+        {
+
+            if (OnKeyInputReceived != null && Input.inputString != "")
+                OnKeyInputReceived.Invoke(Input.inputString[Input.inputString.Length - 1]);
+
+        }
+    }
+}
---
+++ Assets/Scripts/UIKeyboard.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class UIKeyboard : MonoBehaviour
+{
+
+    private List<UIKey> keys = new List<UIKey>();
+
+    void OnEnable()
+    {
+        KeyBoardInputListener.OnKeyInputReceived += HighlightKeyEntered;
+    }
+
+    void OnDisable()
+    {
+        KeyBoardInputListener.OnKeyInputReceived -= HighlightKeyEntered;
+    }
+
+    void Awake()
+    {
+        foreach (Transform child in transform)
+        {
+            keys.Add(new UIKey(child.name[0], child.GetComponent<SpriteRenderer>()));
+        }
+    }
+
+    void HighlightKeyEntered(char keyId)
+    {
+        StartCoroutine(HighlightKeyForSeconds(.05f, keyId));
+    }
+
+    public IEnumerator HighlightKeyForSeconds(float seconds, char keyId)
+    {
+        UIKey key = FindKey(keyId);
+        key.HighlightKey(Color.blue);
+        yield return new WaitForSeconds(seconds);
+        key.HighlightKey(Color.white);
+    }
+
+    UIKey FindKey(char keyId)
+    {
+        return keys.Find(key => key.Id.Equals(keyId));
+    }
+
+
+
+
+
+
+
+    //public Text textString;
+
+    //private char _keyEntered;
+    //private char _currentLetter;
+    //private int _counter = 0;
+
+    //void Update ()
+    //{
+
+    //    string textStream = textString.text;
+
+    //    if (_counter != textStream.Length)
+    //    {
+    //        _currentLetter = textStream[_counter];
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("You won");
+    //    }
+
+    //    foreach (char c in Input.inputString)
+    //    {
+    //        _keyEntered = c;
+    //    }
+
+    //    if (Input.anyKeyDown)
+    //    {
+    //        if (_keyEntered == _currentLetter)
+    //        {
+    //            textString.text = textString.text.Insert(_counter, "<color=green>");
+    //            textString.text = textString.text.Insert(_counter + 14, "</color>");
+    //            _counter += 14 + 8;
+    //        }
+    //        else
+    //        {
+    //            textString.text = textString.text.Insert(_counter, "<color=red>");
+    //            textString.text = textString.text.Insert(_counter + 12, "</color>");
+    //            _counter += 12 + 8;
+    //        }
+    //    }
+    //}
+}
+
+internal class UIKey
+{
+    private SpriteRenderer _sprite;
+    private char _id;
+
+    public UIKey(char id, SpriteRenderer spRenderer)
+    {
+        _id = id;
+        _sprite = spRenderer;
+    }
+
+    public void HighlightKey(Color tint)
+    {
+        _sprite.color = tint;
+     
+    }
+
+    public char Id {
+        get { return _id; }
+    }
+}
