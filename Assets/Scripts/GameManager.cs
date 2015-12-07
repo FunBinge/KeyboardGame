92985d0dd1be8cff86e8c65b9469a296dc9a3aa8
Ed <cardenas.eduardo10@yahoo.com.au>
2015-12-07 14:25:16 +1030

    FirstCommit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+
+public class GameManager : MonoBehaviour {
+
+    public enum GameState
+    {
+        PAUSED,
+        IN_GAME,
+    }
+
+    public static GameState CurrentGameState = GameState.PAUSED;
+
+    public static GameManager SSingleton = null;
+
+    public static GameManager Instance
+    {
+        get { return SSingleton ?? (SSingleton = new GameObject("GameManager").AddComponent<GameManager>()); }
+        
+    }
+
+    void Awake()
+    {
+        if (SSingleton)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            SSingleton = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        CurrentGameState = GameState.IN_GAME;
+    }
+
+
+
+	
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    public delegate void KeyPressedAction(char keyEntered);
+    public static event KeyPressedAction OnKeyboardInputReceived;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.CurrentGameState == GameManager.GameState.IN_GAME && Input.anyKeyDown && !Input.GetMouseButtonDown(0) && !Input.GetMouseButtonDown(1))
+        {
+            if (OnKeyboardInputReceived != null)
+                OnKeyboardInputReceived.Invoke(Input.inputString[Input.inputString.Length]);
+        }
+    }
+}
